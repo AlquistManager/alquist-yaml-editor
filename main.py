cbec210aa4cf9bfce28693c8408de7465afa133f
pritzvac <vpritzl@gmail.com>
2016-12-19 16:19:45 +0100

    Bugfix, code optimization
    Fixed bugs with reading yaml files, removed unnecessary code, improved
    code structure

--- main.py
+++ main.py
@@ -3,29 +3,24 @@ from loaded_states import state_dict, intent_transitions
 import collections
 import io
 from os import listdir
-from os.path import isfile, join
 
 YamlParser()
 
-print(state_dict.keys())
+statePositions = {}
+unreachableNodes = []
+files = []
+posCount = 0
 
 botName = "demo_tel"
 
 states = state_dict[botName]["states"].keys()
-
 next_state = state_dict[botName]["states"]["init"]["transitions"]["next_state"]
 
+#first graph node
 id = 1
-# nodes = {"init": {"id": id, "level": 1}}
-nodes = collections.OrderedDict({"init": {"id": id,
-                                          "level": 1,
-                                          "responses": state_dict[botName]["states"]["init"]["properties"][
-                                              "responses"]}})
+nodes = collections.OrderedDict({"init": {"id": id}})
 current_state = "init"
 edges = []
-
-count = 0
-
 
 # recursive method for graph construction
 def findNextState(current_state, next_state):
@@ -34,40 +29,16 @@ def findNextState(current_state, next_state):
         return
     global id
     id += 1
-    level = nodes[current_state]["level"] + 1
 
-    nodes[next_state] = {"id": id, "level": level}
+    nodes[next_state] = {"id": id}
 
-    edges.append((nodes[current_state]["id"], nodes[next_state]["id"]))
+    if current_state is not None:
+        edges.append((nodes[current_state]["id"], nodes[next_state]["id"]))
 
     current_state = next_state
 
     transitions = state_dict[botName]["states"][current_state]["transitions"]
     properties = state_dict[botName]["states"][current_state]["properties"]
-
-    global count
-
-    nodes[current_state]["responses"] = []
-    if "responses" in properties.keys():
-        for res in properties["responses"]:
-            nodes[current_state]["responses"].append(res)
-        count += 1
-
-    if "text" in properties.keys():
-        nodes[current_state]["responses"].append(properties["text"])
-        count += 1
-
-    if "buttons" in properties.keys():
-        buttons = properties["buttons"]
-        for b in buttons:
-            nodes[current_state]["responses"].append(b["label"] + " ")
-        count += 1
-
-    if "checkboxes" in properties.keys():
-        checkboxes = properties["checkboxes"]
-        for c in checkboxes:
-            nodes[current_state]["responses"].append(c["label"] + " ")
-        count += 1
 
     if "buttons" in properties.keys():
         for button in properties["buttons"]:
@@ -82,108 +53,14 @@ def findNextState(current_state, next_state):
             findNextState(current_state, next_state)
     else:
         for state in transitions.values():
-            #if state != "":
                 findNextState(current_state, state)
-
-
-
-# delete nodes and edges in vis.js javascript file
-def clearGraph():
-    writing = True
-    file = open("graph.js", "r+")
-
-    lines = []
-    for line in file:
-        lines.append(line)
-
-    file.seek(0)
-
-    for line in lines:
-        if "var nodes" in line:
-            writing = False
-            file.write(line)
-        elif "var edges" in line:
-            writing = False
-            file.write(line)
-        elif writing == False and "]);" in line:
-            writing = True
-
-        if writing:
-            file.write(line)
-
-    file.truncate()
-    file.close()
-
-
-# writes nodes and edges of graph to a javascript file for vis.js
-def writeNodesAndEdges(nodes, edges):
-    file = open("graph.js", "r+")
-
-    lines = []
-    for line in file:
-        lines.append(line)
-    file.seek(0)
-
-    for line in lines:
-        if "var nodes" in line:
-            file.write(line)
-
-            # sorted_nodes = sorted(nodes.items(), key=lambda nodes: nodes[1]["level"])
-
-            # print(sorted_nodes)
-
-            for n in nodes:  # {id: 1, label: 'Node 1'},
-                file.write("{id: %d, label: \'%s\'},\n" % (nodes[n]["id"], n))
-                # file.write("{id: %d, label: \'%s\', level: %d, y: %d},\n" % (nodes[n]["id"], n, nodes[n]["level"], nodes[n]["level"]*100))
-                # file.write("{id: %d, label: \'%s\', level: %d},\n" % (n[1]["id"], n[0], n[1]["level"]))
-        elif "var edges" in line:
-            file.write(line)
-            for e in edges:
-                file.write("{from: %d, to: %d, arrows: \'to\'},\n" % (e[0], e[1]))  # {from: 1, to: 3},
-        else:
-            file.write(line)
-    file.truncate()
-    file.close()
-
-
-# creates JSON file for D3
-def writeNodesAndEdgesJSON(nodes, edges):
-    file = open("graph.json", "w")
-
-    file.write("{\n  \"nodes\": [\n")
-
-    i = 0
-    for n in nodes:
-        i += 1
-        file.write("{\"id\": \"%d\", \"name\": \"%s\", \"level\": \"%d\"}" % (nodes[n]["id"], n, nodes[n]["level"]))
-        if i == len(nodes):
-            file.write("\n")
-        else:
-            file.write(",\n")
-
-    file.write("  ],\n\"links\": [\n")
-
-    i = 0
-    for e in edges:
-        i += 1
-        file.write("{\"source\": \"%d\", \"target\": \"%d\"}" % (e[0], e[1]))
-        if i == len(edges):
-            file.write("\n")
-        else:
-            file.write(",\n")
-
-    file.write("  ]\n}")
-
-    file.truncate()
-    file.close()
-
 
 # creates a file to be displayed by viz.js
 def writeGraphVizJs(nodes, edges):
     file = open("graph_viz.txt", "w")
     file.write("digraph G{\n")
 
-
+    #displays nodes in clusters according to yaml files
     clusterNum = 0
     for f in files:
         file.write("subgraph cluster_%d {\n" % (clusterNum))
@@ -197,7 +74,6 @@ def writeGraphVizJs(nodes, edges):
         file.write("style=filled;")
         file.write("color=lightgray;")
         file.write("}\n\n")
-
         clusterNum += 1
 
 
@@ -207,6 +83,7 @@ def writeGraphVizJs(nodes, edges):
     file.write("\n");
     '''
 
+    #list of edges
     for e in edges:
         file.write("%s -> %s;\n" % (findNodeNameById(e[0]), findNodeNameById(e[1])))
 
@@ -215,19 +92,13 @@ def writeGraphVizJs(nodes, edges):
     file.truncate()
     file.close()
 
-
 # searches for a node by its ID
 def findNodeNameById(id):
     for n in nodes:
         if nodes[n]["id"] == id:
             return n
 
-
-statePositions = {}
-files = []
-posCount = 0
-
-
+# finds position of states in yaml files and saves them to statePositions
 def findStatePositions():
     yaml_folder = "bots/" + botName + "/flows/"
     global statePositions
@@ -238,25 +109,33 @@ def findStatePositions():
         if fileName not in files:
             files.append(fileName)
         with io.open(yaml_folder + fileName, "r", encoding="utf-8") as file:
-            file.readline()
             line = file.readline()
             lineNum = 1
+            while "states" not in line:
+                line = file.readline()
+                lineNum += 1
 
             while line:
                 line = file.readline()
                 if not line:
                     break
+                line = line.strip()
+                if not line:
+                    line += "\n"
                 lineNum += 1
-                stateRead = line.strip()[0:-1]
+                stateRead = line.split(":")[0].strip()
                 stateStart = lineNum
                 while line not in ['\n', '\r\n'] and line:
-                    line = file.readline()
+                    line = file.readline().strip()
                     lineNum += 1
+                    if not line:
+                        line += "\n"
                 stateEnd = lineNum
                 if stateStart != stateEnd:
                     statePositions[stateRead] = [fileName, stateStart, stateEnd]
                     posCount += 1
 
+# creates a file with positions of states in yaml files
 def writeStatePositions(statePositions):
     with io.open("state_positions.txt", "w", encoding="utf-8") as file:
         file.write(botName + "\n")
@@ -264,7 +143,7 @@ def writeStatePositions(statePositions):
             file.write("%s %s %d %d\n" % (state, statePositions[state][0], statePositions[state][1], statePositions[state][2]))
         file.close()
 
-unreachableNodes = []
+# finds unreachable nodes and saves them to unreachableNodes
 def findUnreachableNodes():
     global unreachableNodes
     for state in statePositions:
@@ -272,75 +151,22 @@ def findUnreachableNodes():
             unreachableNodes.append(state)
 
 
+print("Bots available: %s" % state_dict.keys())
+print("Creating graph for bot: %s" % botName)
 
 findNextState(current_state, next_state)
-clearGraph()
-writeNodesAndEdges(nodes, edges)
-writeNodesAndEdgesJSON(nodes, edges)
-#writeGraphVizJs(nodes, edges)
-
-print(len(nodes))
-print(len(edges))
-# print(state_dict[botName]["states"]["select_type_test_2"]["transitions"])
-# print(state_dict[botName]["states"]["init"]["properties"]["responses"])
-#print(state_dict[botName])
-#print(count)
-# print(nodes)
-# print(edges)
-
-
 findStatePositions()
 findUnreachableNodes()
 
-'''
+# adds unreachable nodes to the graph
 for pos in statePositions:
     if pos not in nodes.keys():
-        print(pos + " " + statePositions[pos][0])
-'''
-
-
-for pos in statePositions:
-    if pos not in nodes.keys():
-        id += 1
-        nodes[pos] = {"id": id, "level": 0}
-        transitions = state_dict[botName]["states"][pos]["transitions"]
-        properties = state_dict[botName]["states"][pos]["properties"]
-        if "buttons" in properties.keys():
-            for button in properties["buttons"]:
-                if "next_state" in button.keys():
-                    next_state = button["next_state"]
-                    if next_state != "":
-                        findNextState(pos, next_state)
-
-        if "next_state" in transitions.keys():
-            next_state = transitions["next_state"]
-            if next_state != "":
-                findNextState(pos, next_state)
-        else:
-            for state in transitions.values():
-                findNextState(pos, state)
-
-#print(len(nodes))
-
-def detectNodesWithoutInputs():
-    noInputs = []
-    inputDetected = False
-    for n in nodes:
-        inputDetected = False
-        for e in edges:
-            if e[1] == nodes[n]["id"]:
-                inputDetected = True
-                break;
-        if not inputDetected:
-            noInputs.append(n)
-    print(noInputs)
-
-#detectNodesWithoutInputs()
+        findNextState(None, pos)
 
 writeGraphVizJs(nodes, edges)
-
 writeStatePositions(statePositions)
 
-print(unreachableNodes)
+print("Graph nodes: %d" % len(nodes))
+print("Graph edges: %d" % len(edges))
 
 
