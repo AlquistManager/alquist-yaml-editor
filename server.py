175ab4bfaa58fe7b90b4e36420e9d9fc9f365de4
pritzvac <vpritzl@gmail.com>
2017-01-06 17:32:14 +0100

    Disabled textarea

---
+++ server.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask import request
+app = Flask(__name__)
+
+@app.route('/', methods=['POST', 'GET'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'],
+                       request.form['password']):
+            return log_the_user_in(request.form['username'])
+        else:
+            error = 'Invalid username/password'
+    # the code below is executed if the request method
+    # was GET or the credentials were invalid
+    return render_template('login.html', error=error)
