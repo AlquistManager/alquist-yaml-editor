168f9612d66b184f110b50271f1e3493b99cbda6
pritzvac <vpritzl@gmail.com>
2017-01-20 17:25:43 +0100

    Scaled SVG, start of file tree

--- server.py
+++ server.py
@@ -4,9 +4,13 @@ from crossdomain import *
 import json
 import io
 import main
+import os
+import urllib
+
 app = Flask(__name__)
 
-@app.route('/', methods=['POST', 'GET', 'OPTIONS'])
+
+@app.route('/update', methods=['POST', 'GET', 'OPTIONS'])
 @crossdomain(origin='*')
 def update():
     if request.method == 'POST':
@@ -16,11 +20,48 @@ def update():
     return "OK"
 
 
+@app.route('/', methods=['POST', 'GET', 'OPTIONS'])
+@crossdomain(origin='*')
+def requestFiles():
+    if request.method == 'POST':
+        r = ['<ul class="jqueryFileTree" style="display: none;">']
+        d = request.get_data()
+        print(d)
+
+        r = "<ul>"
+        for f in os.listdir('.'):
+            r += "<il>" + f + "</li>"
+
+        r += "</ul>"
+
+
+        '''
+        try:
+            r = ['<ul class="jqueryFileTree" style="display: none;">']
+            d = request.get_data()
+
+            d = urllib.unquote(request.POST.get('dir', 'c:\\temp'))
+            for f in os.listdir(d):
+                ff = os.path.join(d, f)
+                if os.path.isdir(ff):
+                    r.append('<li class="directory collapsed"><a rel="%s/">%s</a></li>' % (ff, f))
+                else:
+                    e = os.path.splitext(f)[1][1:]  # get .ext and remove dot
+                    r.append('<li class="file ext_%s"><a rel="%s">%s</a></li>' % (e, ff, f))
+            r.append('</ul>')
+        except Exception, e:
+            r.append('Could not load directory: %s' % str(e))
+        r.append('</ul>')
+        return HttpResponse(''.join(r))'''
+    return r
+
+
 def writeToFile(data):
     print("writing file")
     with io.open("bots/" + data["botname"].strip() + "/flows/" + data["filename"], "w", encoding="utf-8") as file:
         file.write(data["text"])
         file.close()
+
 
 def recreateGraph(botName):
     print("recreating graph")
